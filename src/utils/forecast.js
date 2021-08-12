3666fc93ede5342cc534c190c227593ffdec7a83
Prakash M <prakash.m@codematrix.org>
2021-08-12 09:56:29 +0530

    modified

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const forecast=(address, callback)=>{
     // const url='https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(longitude)+','+encodeURIComponent(latitude)+'.json?access_token=pk.eyJ1IjoicHJha3UiLCJhIjoiY2tzNGU4cTA3MjU3cjJ2bXJ3ZHBjNjV6eCJ9.zJSvPrMmJN4xUkleLIvlbA&limit-1'
-    const url = 'http://api.weatherstack.com/current?access_key=60dd26e50b674661906f01ab64120a7d&query='+encodeURIComponent(address)+
+    const url = 'http://api.weatherstack.com/current?access_key=60dd26e50b674661906f01ab64120a7d&query='+encodeURIComponent(address)
     request({url, json:true}, (error, response)=>{
         if(error){
             callback('unable to connect location services!', undefined)
